d89c6b072222924b5a0cab13f96d4e7d7a583adc
Alejandro Valdés <alejandrovaldes@live.com>
2012-06-18 11:24:53 -0500

    prueba de ambientes

--- template/site/Arrays.php
+++ template/site/Arrays.php
@@ -33,7 +33,13 @@ echo $encabezado;
 }
 ?>
 
+<?php 
+  $env = 
+json_decode(file_get_contents("/home/dotcloud/environment.json"), true);
 
+echo "Application Name: {$env['DOTCLOUD_PROJECT']}\n";
+
+?>
 
 </body>
 </html>
