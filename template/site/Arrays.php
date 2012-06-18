67daa55d560cac6303c248474abdabcf5990c749
felipexxx <felipex1991@gmail.com>
2012-06-18 00:29:12 -0500

    template sin imagenes derechos de autor

---
+++ template/site/Arrays.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<META HTTP-EQUIV="Content-Type" content="text/html; charset=utf-8"/>
+ 
+
+<?php
+$variables = $_POST["feed_item_length"];?>
+
+<?php
+$titulo="TITULO DE LA PAGINA ";
+hacer_encabezado($titulo);
+
+?>
+
+<body>
+
+
+
+
+<?php
+$cars[0]="item1";
+$cars[1]="item2";
+$cars[2]="item3";
+$cars[3]="item4"; 
+echo $cars[0] . " and " . $cars[1] . " are Swedish cars.";
+?>
+
+
+<?php
+function hacer_encabezado($titulo)
+{
+$encabezado="<html><head>t<title>$titulo</title></head>";
+echo $encabezado;
+}
+?>
+
+
+
+</body>
+</html>
