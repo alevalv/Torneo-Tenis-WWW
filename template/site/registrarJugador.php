b5c4e2f95a469badfe6aa8b0f87bd3264da4747c
Alejandro Valdés <alejandrovaldes@live.com>
2012-06-20 21:12:58 -0500

    arreglo para registrar

--- template/site/registrarJugador.php
+++ template/site/registrarJugador.php
@@ -1,11 +1,16 @@
 <?php
 	if(isset($_POST['jugador_codigo']) && isset($_POST['jugador_username']) && isset($_POST['jugador_password']) && isset($_POST['jugador_nombre']) && isset($_POST['jugador_sexo']) && isset($_POST['jugador_categoria']) ){
 		include("fachada.php");
+		try{
 		$instancia = new fachada();
 		$db = $instancia->conect();
 		$jugadorCollection = $db->jugador;
 		$newUser = array("jugador_codigo" => $_POST['jugador_codigo'], "jugador_username" => $_POST['jugador_username'], "jugador_password" => $_POST['jugador_password'], "jugador_es_pareja" => false,"jugador_nombre1" => $_POST['jugador_nombre'], "jugador_sexo1" => $_POST['jugador_sexo'], "jugador_categoria" => $_POST['jugador_categoria'], "jugador_ranking" => 1});
 		$jugadorCollection->insert($newUser);
+		}
+		catch (Exception $e) {
+			echo 'Caught exception: ',  $e->getMessage(), "\n";
+		}
 	}
 	echo $_POST['jugador_codigo'];
 	//echo "<br />";
