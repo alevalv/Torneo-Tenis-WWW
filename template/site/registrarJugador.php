707e6ccb86fe8fcb971c609b6e437099e8cd0467
Alejandro Valdés <alejandrovaldes@live.com>
2012-06-20 20:27:06 -0500

    arreglo para registrar

--- template/site/registrarJugador.php
+++ template/site/registrarJugador.php
@@ -1,9 +1,13 @@
 <?php
-	include("fachada.php");
-	$fachada= new fachada();
-	$db = $fachada->conect();
-	$db->jugador->insert({'jugador_codigo': $_POST['jugador_codigo'], 'jugador_username': $_POST['jugador_username'], 'jugador_password':$_POST['jugador_password'],'jugador_es_pareja' : false,'jugador_nombre1':$_POST['jugador_nombre'],'jugador_sexo1': $_POST['jugador_sexo'], 'jugador_categoria':$_POST['jugador_categoria'],'jugador_ranking':1});
-	echo $_POST['jugador_codigo'];
+	if(isset($_POST['jugador_codigo']) && isset($_POST['jugador_username']) && isset($_POST['jugador_password']) && isset($_POST['jugador_nombre']) && isset($_POST['jugador_sexo']) && isset($_POST['jugador_categoria']) ){
+		include("fachada.php");
+		$instancia = new fachada();
+		$db = $instancia->conect();
+		$jugadorCollection = $db->jugador;
+		$newUser = array('jugador_codigo' => $_POST['jugador_codigo'], 'jugador_username' => $_POST['jugador_username'], 'jugador_password' => $_POST['jugador_password'], 'jugador_es_pareja' => false,'jugador_nombre1' => $_POST['jugador_nombre'], 'jugador_sexo1' => $_POST['jugador_sexo'], 'jugador_categoria' => $_POST['jugador_categoria'], 'jugador_ranking' => 1});
+		$db->jugador->insert($newUser);
+	}
+	//echo $_POST['jugador_codigo'];
 	//echo "<br />";
 	//echo $_POST['jugador_username'];
 	//echo "<br />";
