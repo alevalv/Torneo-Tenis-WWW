81398c2a33428ce2e10e9213c3c0c44969aa5fa6
Alejandro Valdés <alejandrovaldes@live.com>
2012-06-20 20:37:04 -0500

    arreglo para registrar

--- template/site/registrarJugador.php
+++ template/site/registrarJugador.php
@@ -4,8 +4,8 @@
 		$instancia = new fachada();
 		$db = $instancia->conect();
 		$jugadorCollection = $db->jugador;
-		$newUser = array('jugador_codigo' => $_POST['jugador_codigo'], 'jugador_username' => $_POST['jugador_username'], 'jugador_password' => $_POST['jugador_password'], 'jugador_es_pareja' => false,'jugador_nombre1' => $_POST['jugador_nombre'], 'jugador_sexo1' => $_POST['jugador_sexo'], 'jugador_categoria' => $_POST['jugador_categoria'], 'jugador_ranking' => 1});
-		$db->jugador->insert($newUser);
+		$newUser = array("jugador_codigo" => $_POST['jugador_codigo'], "jugador_username" => $_POST['jugador_username'], "jugador_password" => $_POST['jugador_password'], "jugador_es_pareja" => false,"jugador_nombre1" => $_POST['jugador_nombre'], "jugador_sexo1" => $_POST['jugador_sexo'], "jugador_categoria" => $_POST['jugador_categoria'], "jugador_ranking" => 1});
+		$jugadorCollection->insert($newUser);
 	}
 	//echo $_POST['jugador_codigo'];
 	//echo "<br />";
