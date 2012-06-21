03af443d1bab8241606f262f5286b9bedfffbe12
Cristina Bustos <mcb9216@hotmail.com>
2012-06-21 18:30:37 -0500

    validador de usernames

---
+++ template/site/controlador/validacionUsername.php
@@ -0,0 +1,24 @@
+<?php
+	include("fachada.php");
+	class ValidadorUsername
+	{
+		public static function validar($username){
+			$instancia = new fachada();
+			$db = $instancia->conect();
+			$colection_Admin = $db->admin;	 
+			$usr_Admin = array( "admin_username" => $username);	 
+			$result_Admin = $colection_Admin->find($usr_Admin);
+			$colection_Juez = $db->juez;
+			$usr_Juez = array( "juez_username" => $username);
+			$result_Juez = $colection_Juez->find($usr_Juez);
+			$colection = $db->jugador;
+			$usr = array( "jugador_username" => $username);	 
+			$result = $colection->find($usr);
+			if($result_Admin->count()==0 && $result_Juez->count()==0 && $result->count()==0){
+				return true;
+			}
+			else{ return false; }
+  	}	
+}
+?>
+
--- template/site/modelo/guardarJugador.php
+++ template/site/modelo/guardarJugador.php
@@ -1,14 +1,20 @@
 <?php
 	if(isset($_POST['jugador_codigo']) && isset($_POST['jugador_username']) && isset($_POST['jugador_password']) && isset($_POST['jugador_nombre']) && isset($_POST['jugador_sexo']) && isset($_POST['jugador_categoria']) ){
 		include("../controlador/fachada.php");
+		include("../controlador/validacionUsername.php");
 		try{
-		$instancia = new fachada();
-		$db = $instancia->conect();
-		$collection= new MongoCollection($db, 'jugador');
-		$newUser = array("jugador_codigo" => $_POST['jugador_codigo'], "jugador_username" => $_POST['jugador_username'], "jugador_password" => $_POST['jugador_password'], "jugador_es_pareja" => false,"jugador_nombre1" => $_POST['jugador_nombre'], "jugador_sexo1" => $_POST['jugador_sexo'], "jugador_categoria" => $_POST['jugador_categoria'], "jugador_ranking" => 1);
-		$collection->insert($newUser);
-		header("Location: ../vista/index.php");
-		exit();
+			$instancia = new fachada();
+			$db = $instancia->conect();
+			$collection= new MongoCollection($db, 'jugador');
+			if(ValidadorUsername::validar($_POST['jugador_username'])){
+				$newUser = array("jugador_codigo" => $_POST['jugador_codigo'], "jugador_username" => $_POST['jugador_username'], "jugador_password" => $_POST['jugador_password'], "jugador_es_pareja" => false,"jugador_nombre1" => $_POST['jugador_nombre'], "jugador_sexo1" => $_POST['jugador_sexo'], "jugador_categoria" => $_POST['jugador_categoria'], "jugador_ranking" => 1);
+				$collection->insert($newUser);
+				header("Location: ../vista/index.php");
+				exit();
+			}
+			else{
+				header("Location: ../vista/registrarJugador.php?errorusuario=si");
+			}
 		}
 		catch (Exception $e) {
 			//echo 'Caught exception: ',  $e->getMessage(), "\n";			
