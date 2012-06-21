98b423ac4494bb9248befaf41324ae4bdeb6d6b2
Alejandro Valdés <alejandrovaldes@live.com>
2012-06-20 22:33:55 -0500

    corregido error

--- template/site/registrarJugador.php
+++ template/site/registrarJugador.php
@@ -5,7 +5,7 @@
 		$instancia = new fachada();
 		$db = $instancia->conect();
 		$collection= new MongoCollection($db, 'jugador');
-		$newUser = array("jugador_codigo" => $_POST['jugador_codigo'], "jugador_username" => $_POST['jugador_username'], "jugador_password" => $_POST['jugador_password'], "jugador_es_pareja" => false,"jugador_nombre1" => $_POST['jugador_nombre'], "jugador_sexo1" => $_POST['jugador_sexo'], "jugador_categoria" => $_POST['jugador_categoria'], "jugador_ranking" => 1});
+		$newUser = array("jugador_codigo" => $_POST['jugador_codigo'], "jugador_username" => $_POST['jugador_username'], "jugador_password" => $_POST['jugador_password'], "jugador_es_pareja" => false,"jugador_nombre1" => $_POST['jugador_nombre'], "jugador_sexo1" => $_POST['jugador_sexo'], "jugador_categoria" => $_POST['jugador_categoria'], "jugador_ranking" => 1);
 		$collection->insert($newUser);
 		}
 		catch (Exception $e) {
