c2ab4e3b395685c4ab769f726a4b189f72d9504b
themonki <edgarandres29@gmail.com>
2012-06-21 19:13:57 -0500

    redireccion en registrar torneo

--- template/site/modelo/registrarTorneo.php
+++ template/site/modelo/registrarTorneo.php
@@ -7,9 +7,13 @@
 		$collection= new MongoCollection($db, 'torneo');
 		$newUser = array("torneo_lugar" => $_POST['torneo_lugar'], "torneo_nombre" => $_POST['torneo_nombre'], "torneo_num_canchas" => $_POST['torneo_num_canchas'] , "torneo_num_jugadores" => $_POST['torneo_num_jugadores'], "torneo_jugadores_registrados" => 0, "torneo_fecha_inicio" => $_POST['torneo_fecha_inicio'], "torneo_fecha_fin" => $_POST['torneo_fecha_fin'] , "torneo_categoria" => $_POST['torneo_categoria'], "torneo_modalidad" => $_POST['torneo_modalidad']);
 		$collection->insert($newUser);
+		header("Location: ../vista/index.php");
+		exit();
 		}
 		catch (Exception $e) {
 			echo 'Caught exception: ',  $e->getMessage(), "\n";
 		}
+			header("Location: ../vista/registrarPareja.php?errorusuario=si");
+
 	}
 ?>
