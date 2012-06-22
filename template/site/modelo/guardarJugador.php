4afb9babeff4ef4194e109da152b45d0319423cf
Alejandro Valdés <alejandrovaldes@live.com>
2012-06-21 20:45:44 -0500

    comentado lo de comprobar usuario

--- template/site/modelo/guardarJugador.php
+++ template/site/modelo/guardarJugador.php
@@ -1,12 +1,15 @@
 <?php
 	if(isset($_POST['jugador_codigo']) && isset($_POST['jugador_username']) && isset($_POST['jugador_password']) && isset($_POST['jugador_nombre']) && isset($_POST['jugador_sexo']) && isset($_POST['jugador_categoria']) ){
 		include("../controlador/fachada.php");
-		include("../controlador/ValidadorUsername.php");
+		//include("../controlador/ValidadorUsername.php");
 		try{
 			$instancia = new fachada();
 			$db = $instancia->conect();
 			$collection= new MongoCollection($db, 'jugador');
-			if(ValidadorUsername::validar($_POST['jugador_username'])){
+			//echo "hola";
+			//echo "<br />";
+			//echo ValidadorUsername::validar($_POST['jugador_username']);
+			if(true){
 				echo "sali";
 				$newUser = array("jugador_codigo" => $_POST['jugador_codigo'], "jugador_username" => $_POST['jugador_username'], "jugador_password" => $_POST['jugador_password'], "jugador_es_pareja" => false,"jugador_nombre1" => $_POST['jugador_nombre'], "jugador_sexo1" => $_POST['jugador_sexo'], "jugador_categoria" => $_POST['jugador_categoria'], "jugador_ranking" => 1);
 				$collection->insert($newUser);
