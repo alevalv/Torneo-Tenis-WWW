af020c6f088ee76afc7e9a54d69f04c200484629
felipexxx <felipex1991@gmail.com>
2012-06-25 00:26:33 -0500

    nuevo dato a torneo

--- template/site/modelo/registrarTorneo.php
+++ template/site/modelo/registrarTorneo.php
@@ -11,7 +11,7 @@
 		$t_codigo = $codigos->current();
 		
 		
-		$newUser = array("torneo_codigo" => $t_codigo['torneo_codigo'], "torneo_lugar" => $_POST['torneo_lugar'], "torneo_nombre" => $_POST['torneo_nombre'], "torneo_num_canchas" => $_POST['torneo_num_canchas'] , "torneo_num_jugadores" => $_POST['torneo_num_jugadores'], "torneo_jugadores_registrados" => 0, "torneo_fecha_inicio" => $_POST['torneo_fecha_inicio'], "torneo_fecha_fin" => $_POST['torneo_fecha_fin']);
+		$newUser = array("torneo_codigo" => $t_codigo['torneo_codigo'], "torneo_lugar" => $_POST['torneo_lugar'], "torneo_nombre" => $_POST['torneo_nombre'], "torneo_num_canchas" => $_POST['torneo_num_canchas'] , "torneo_num_jugadores" => $_POST['torneo_num_jugadores'], "torneo_jugadores_registrados" => 0, "torneo_fecha_inicio" => $_POST['torneo_fecha_inicio'], "torneo_fecha_fin" => $_POST['torneo_fecha_fin'],"tiene_cronograma" => "no" );
 		$collection->insert($newUser);
 		header("Location: ../vista/index.php");
 		exit();
