1cc46a834bf7f8ea70dec39617d2bf3fd6b0f3a6
Alejandro Valdés <alejandrovaldes@live.com>
2012-06-25 17:17:05 -0500

    agregados campos de dia mes y año para las fechas

--- template/site/modelo/registrarTorneo.php
+++ template/site/modelo/registrarTorneo.php
@@ -1,5 +1,5 @@
 <?php
-	if(isset($_POST['torneo_nombre']) && isset($_POST['torneo_lugar']) && isset($_POST['torneo_num_canchas']) && isset($_POST['torneo_num_jugadores']) && isset($_POST['torneo_fecha_inicio']) && isset($_POST['torneo_fecha_fin'])){
+	if(isset($_POST['torneo_nombre']) && isset($_POST['torneo_lugar']) && isset($_POST['torneo_num_canchas']) && isset($_POST['torneo_num_jugadores']) && isset($_POST['torneo_fecha_inicio_dia'])  && isset($_POST['torneo_num_jugadores']) && isset($_POST['torneo_fecha_inicio_mes'])  && isset($_POST['torneo_num_jugadores']) && isset($_POST['torneo_fecha_inicio_anio']) && isset($_POST['torneo_fecha_fin_dia']) && isset($_POST['torneo_fecha_fin_mes']) && isset($_POST['torneo_fecha_fin_anio'])){
 		include("../controlador/fachada.php");
 		try{
 		$instancia = new fachada();
@@ -11,7 +11,7 @@
 		$t_codigo = $codigos->current();
 		
 		
-		$newUser = array("torneo_codigo" => $t_codigo['torneo_codigo'], "torneo_lugar" => $_POST['torneo_lugar'], "torneo_nombre" => $_POST['torneo_nombre'], "torneo_num_canchas" => $_POST['torneo_num_canchas'] , "torneo_num_jugadores" => $_POST['torneo_num_jugadores'], "torneo_jugadores_registrados" => 0, "torneo_fecha_inicio" => $_POST['torneo_fecha_inicio'], "torneo_fecha_fin" => $_POST['torneo_fecha_fin'],"tiene_cronograma" => "no" );
+		$newUser = array("torneo_codigo" => $t_codigo['torneo_codigo'], "torneo_lugar" => $_POST['torneo_lugar'], "torneo_nombre" => $_POST['torneo_nombre'], "torneo_num_canchas" => $_POST['torneo_num_canchas'] , "torneo_num_jugadores" => $_POST['torneo_num_jugadores'], "torneo_jugadores_registrados" => 0, "torneo_fecha_inicio_dia" => $_POST['torneo_fecha_inicio_dia'], "torneo_fecha_inicio_mes" => $_POST['torneo_fecha_inicio_mes'], "torneo_fecha_inicio_anio" => $_POST['torneo_fecha_inicio_anio'], "torneo_fecha_fin_dia" => $_POST['torneo_fecha_fin_dia'], "torneo_fecha_fin_mes" => $_POST['torneo_fecha_fin_mes'], "torneo_fecha_fin_anio" => $_POST['torneo_fecha_fin_anio'],"tiene_cronograma" => "no" );
 		$collection->insert($newUser);
 		header("Location: ../vista/index.php");
 		exit();
