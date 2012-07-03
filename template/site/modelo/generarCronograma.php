f71842e1df9033442f39621ba94c01abb36f05b0
felipexxx <felipex1991@gmail.com>
2012-07-02 19:12:06 -0500

    cronograma

--- template/site/modelo/generarCronograma.php
+++ template/site/modelo/generarCronograma.php
@@ -9,7 +9,13 @@
         echo '<p>'.'entre'.$partidos.$combo.'</p>';
         
         $arraySubTorneos = generarArraySubTorneos($combo);// en este array tendremos el id del subtorneo y tendremos 'si' si supero el minimo de jugadores y no de los contrario 
-        var_dump($arraySubTorneos);
+        
+        $Torneo =getMinimoParaGenerarTorneo($combo);//es el valor que se ingresa al crear el torneo y sirve para decidir si un torneo se lleva a cabo, esto se hace distinguiendo modalidad categoria teniedo en cuenta que estos generan otro torneo
+        $minimoParaGenerarTorneo=$Torneo["torneo_num_jugadores"];
+        
+        echo 'numero::'.$minimoParaGenerarTorneo;
+        
+        
         
     } catch (Exception $e) {
         echo 'Caught exception: ', $e->getMessage(), "\n";
