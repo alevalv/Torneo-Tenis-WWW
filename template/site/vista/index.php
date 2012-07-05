15d79ab022ff7e7b86c8f8fae45280a63cd50302
Cristina Bustos <mcb9216@hotmail.com>
2012-07-05 04:52:01 -0500

    mejoras interfaz grafica normal

--- template/site/vista/index.php
+++ template/site/vista/index.php
@@ -50,8 +50,8 @@
 				
 					<div class="slider">
 					  <ul class="items">
-						<li><img src="images/torneo12.jpg" alt=""></li>
-						<li><img src="images/torneo22.jpg" alt=""></li>
+						<li><img src="images/banner1.jpg" alt="" align="center"></li>
+						<li><img src="images/banner2.jpg" alt="" align="center"></li>
 						<!--<li><img src="images/img3.jpg" alt=""></li>
 						<li><img src="images/img4.jpg" alt=""></li>-->
 					  </ul>
